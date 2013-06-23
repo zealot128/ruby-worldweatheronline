b9ad5ecb27a2bd321bb847d759953540ccde6112
Stefan Wienert <stwienert@gmail.com>
2013-06-23 23:20:00 +0200

    ..

--- run.rb
+++
@@ -1,7 +0,0 @@
-require "worldweatheronline"
-
-Worldweatheronline.api_key =  "2yq6s9seh6fah6eu6tzccxrg"
-
-
-@r= Worldweatheronline::FreeWeather.weather q: "51.0,13.0", num_of_days: 5, includelocation: "yes"
-p @r
